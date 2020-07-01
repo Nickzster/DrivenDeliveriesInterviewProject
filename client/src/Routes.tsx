b48ce46b59f13146877df66801b4cbeb2a4b8f40
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-07-01 12:57:28 -0700

    Got the board running in 8x8 dimmensions.

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -2,11 +2,12 @@ import { Switch, Route } from "react-router-dom";
 import React from "react";
 //Import your components here
 import TestScreen from "./screens/TestScreen";
+import Board from "./screens/Checkboard";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path='/' component={TestScreen} />
+      <Route exact path='/' component={Board} />
       <Route exact path='/test-screen' component={TestScreen} />
       {/* <Route component={NotFound} />  */}
     </Switch>
--- client/src/screens/Checkboard/Board.tsx
+++ client/src/screens/Checkboard/Board.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/board.css";
+import { board } from "./VirtualBoard";
 
 const Board = () => {
-  return <div></div>;
+  const [boardSize, updateBoardSize] = useState([
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ]);
+
+  return (
+    <div className='screen'>
+      <div className='board'>
+        {boardSize.map((rows) => (
+          <div className='rows'>
+            {rows.map((element) => (
+              <p className='element'>{element}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
